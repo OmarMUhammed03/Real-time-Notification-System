1a2fa45c1896bd095e7d943f86a97a15759e48b7
omar mohammed <omarb2515@gmail.com>
2025-05-23 19:50:20 +0300

    add api-gateway

---
+++ api-gateway/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const proxy = require("./routes/proxy");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const colors = require("colors");
+const http = require("http");
+const connectDB = require("../utils/functions");
+
+const app = express();
+const server = http.createServer(app);
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(logger("dev"));
+
+app.use("/api", proxy);
+
+connectDB().then(() => console.log("Connected to MongoDB 200 OK".bgGreen.bold));
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`.cyan.bold);
+});
---
+++ api-gateway/routes/proxy.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const { authenticate } = require("../middlewares/authMiddleWare");
+
+const router = express.Router();
+
+const AUTH_SERVICE_PORT = process.env.AUTH_SERVICE_PORT;
+const AUTH_SERVICE_URL = `http://localhost:${AUTH_SERVICE_PORT}`;
+
+const USER_SERVICE_PORT = process.env.USER_SERVICE_PORT;
+const USER_SERVICE_URL = `http://localhost:${USER_SERVICE_PORT}`;
+
+const NOTIFICATION_SERVICE_PORT = process.env.NOTIFICATION_SERVICE_PORT;
+const NOTIFICATION_SERVICE_URL = `http://localhost:${NOTIFICATION_SERVICE_PORT}`;
+
+router.use(
+  "/auth",
+  createProxyMiddleware({
+    target: AUTH_SERVICE_URL,
+    changeOrigin: true,
+  })
+);
+
+router.use(
+  "/users",
+  authenticate,
+  createProxyMiddleware({
+    target: USER_SERVICE_URL,
+    changeOrigin: true,
+  })
+);
+
+router.use(
+  "/notifications",
+  authenticate,
+  createProxyMiddleware({
+    target: NOTIFICATION_SERVICE_URL,
+    changeOrigin: true,
+  })
+);
+
+module.exports = router;

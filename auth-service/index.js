a9898d7e20abe62f7e9e175fdbe8cd6e81258939
omar mohammed <omarb2515@gmail.com>
2025-05-24 18:26:31 +0300

    add db connection

--- auth-service/index.js
+++ auth-service/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const colors = require("colors");
 
 const app = express();
 const authRouter = require("./controllers/authController");
 const { HTTP_STATUS } = require("./utils/constants");
+const { connectDB } = require("./utils/functions");
+
+connectDB(process.env.MONGO_URI).then(() =>
+  console.log("Connected to MongoDB 200 OK".bgGreen.bold)
+);
 
 app.use(express.json());
 app.use("/auth", authRouter);

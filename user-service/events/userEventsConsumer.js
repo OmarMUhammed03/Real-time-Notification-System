780e1fe03b7ec66b5e3f2afc7767c187adb0256b
omar mohammed <omarb2515@gmail.com>
2025-05-26 00:51:27 +0300

    fix kafka consumer

--- user-service/events/userEventsConsumer.js
+++ user-service/events/userEventsConsumer.js
@@ -2,14 +2,36 @@ const { createConsumer, startConsumer } = require("../utils/kafkaConsumer");
 const userService = require("../services/userService");
 
 async function handleUserRegistered({ message }) {
-  const event = JSON.parse(message.value.toString());
-  const { userId, ...body } = event;
-  await userService.createUser({ _id: userId, ...body });
+  try {
+    const event = JSON.parse(message.value.toString());
+    const { userId, ...body } = event;
+    await userService.createUser({ _id: userId, ...body });
+  } catch (err) {
+    console.error("Error handling userRegistered event:", err.message);
+  }
 }
 
 async function startUserEventsConsumer() {
   const consumer = createConsumer("user-service-group");
-  await startConsumer(consumer, "userRegistered", handleUserRegistered);
+
+  try {
+    await startConsumer(consumer, "userRegistered", handleUserRegistered);
+    console.log("Kafka consumer started for 'userRegistered' events");
+
+    process.on("SIGINT", async () => {
+      console.log("Shutting down consumer...");
+      await consumer.disconnect();
+      process.exit(0);
+    });
+
+    process.on("SIGTERM", async () => {
+      console.log("Shutting down consumer...");
+      await consumer.disconnect();
+      process.exit(0);
+    });
+  } catch (err) {
+    console.error("Failed to start user events consumer:", err.message);
+  }
 }
 
 startUserEventsConsumer();
--- user-service/utils/kafkaConsumer.js
+++ user-service/utils/kafkaConsumer.js
@@ -5,13 +5,18 @@ function createConsumer(groupId) {
 }
 
 async function startConsumer(consumer, topic, onMessage) {
-  await consumer.connect();
-  await consumer.subscribe({ topic, fromBeginning: false });
-  await consumer.run({
-    eachMessage: async ({ topic, partition, message }) => {
-      await onMessage({ topic, partition, message });
-    },
-  });
+  try {
+    await consumer.connect();
+    await consumer.subscribe({ topic, fromBeginning: false });
+    await consumer.run({
+      eachMessage: async ({ topic, partition, message }) => {
+        await onMessage({ topic, partition, message });
+      },
+    });
+  } catch (err) {
+    console.error(`Kafka consumer error (topic: ${topic}):`, err.message);
+  }
 }
+
 
 module.exports = { createConsumer, startConsumer };

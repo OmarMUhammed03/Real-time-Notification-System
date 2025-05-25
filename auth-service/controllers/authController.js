32c2592b5aae666842edfa9d1f54d8e04880757c
omar mohammed <omarb2515@gmail.com>
2025-05-26 02:45:34 +0300

    add access&refersh tokens in cookies & user details in headers

--- auth-service/controllers/authController.js
+++ auth-service/controllers/authController.js
@@ -1,40 +1,75 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authService = require('../services/authService');
-const { HTTP_STATUS } = require('../utils/constants');
+const authService = require("../services/authService");
+const { HTTP_STATUS } = require("../utils/constants");
 
-router.post('/register', async (req, res, next) => {
+router.get("/me", async (req, res, next) => {
+  try {
+    const { id, email } = await authService.getCurrentUser(
+      req.cookies.access_token
+    );
+    res.setHeader("X-User-Id", id);
+    res.setHeader("X-User-Email", email);
+    res.status(HTTP_STATUS.OK).json({ id, email });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/register", async (req, res, next) => {
   try {
     const user = await authService.register(req.body);
-    res.status(HTTP_STATUS.CREATED).json({ message: 'User registered', userId: user._id });
+    res
+      .status(HTTP_STATUS.CREATED)
+      .json({ message: "User registered", userId: user._id });
   } catch (err) {
     next(err);
   }
 });
 
-router.post('/login', async (req, res, next) => {
+router.post("/login", async (req, res, next) => {
   try {
-    const result = await authService.login(req.body);
-    res.status(HTTP_STATUS.OK).json(result);
+    const { token, refreshToken } = await authService.login(req.body);
+    res.cookie("access_token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: process.env.ACCESS_TOKEN_MAX_AGE,
+    });
+    res.cookie("refresh_token", refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: process.env.REFRESH_TOKEN_MAX_AGE,
+    });
+    res.status(HTTP_STATUS.OK).json({ message: "Login successful" });
   } catch (err) {
     next(err);
   }
 });
 
-router.post('/refresh-token', async (req, res, next) => {
+router.post("/refresh-token", async (req, res, next) => {
   try {
     const { refreshToken: token } = req.body;
     const result = await authService.refreshToken(token);
+    res.cookie("access_token", result.token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: process.env.ACCESS_TOKEN_DURATION,
+    });
     res.status(HTTP_STATUS.OK).json(result);
   } catch (err) {
     next(err);
   }
 });
 
-router.post('/logout', async (req, res, next) => {
+router.post("/logout", async (req, res, next) => {
   try {
     const { refreshToken: token } = req.body;
     const result = await authService.logout(token);
+    res.clearCookie("access_token");
+    res.clearCookie("refresh_token");
     res.status(HTTP_STATUS.OK).json(result);
   } catch (err) {
     next(err);

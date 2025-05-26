a4aa1e055bca74966b576d854d4cdca17128607f
OmarMuhammed03 <omarb2515@gmail.com>
2025-05-26 20:38:10 +0300

    update notification service

--- notification-service/services/notificationService.js
+++ notification-service/services/notificationService.js
@@ -5,7 +5,8 @@ exports.createNotification = async (notification) => {
   if (
     !notification.receiverId ||
     !notification.senderId ||
-    !notification.message ||
+    !notification.content ||
+    !notification.title ||
     !notification.category
   ) {
     const error = new Error("Missing required fields");
@@ -17,3 +18,29 @@ exports.createNotification = async (notification) => {
   );
   return createdNotification;
 };
+
+exports.findNotificationsBySenderId = async (senderId) => {
+  const notifications = await notificationRepository.getSentNotifications(senderId);
+  return notifications;
+};
+
+exports.findNotificationsByReceiverId = async (receiverId) => {
+  const notifications = await notificationRepository.getNotifications(receiverId);
+  return notifications;
+};
+
+exports.updateNotification = async (notificationId, update) => {
+  const updatedNotification = await notificationRepository.updateNotification(
+    notificationId,
+    update
+  );
+  return updatedNotification;
+};
+
+exports.deleteNotification = async (notificationId) => {
+  const deletedNotification = await notificationRepository.deleteNotification(
+    notificationId
+  );
+  return deletedNotification;
+};
+

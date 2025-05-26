1602dbe284630362930a685f97a3caf83bf9a8c6
OmarMuhammed03 <omarb2515@gmail.com>
2025-05-26 20:37:18 +0300

    add notification controller

---
+++ notification-service/controllers/notificationController.js
@@ -0,0 +1,54 @@
+const express = require("express");
+
+const notificationService = require("../services/notificationService");
+const { HTTP_STATUS } = require("../utils/constants");
+
+const router = express.Router();
+
+router.get("/senderId/:senderId", async (req, res, next) => {
+  try {
+    const notifications = await notificationService.findNotificationsBySenderId(
+      req.params.senderId
+    );
+    res.status(HTTP_STATUS.OK).json(notifications);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/receiverId/:receiverId", async (req, res, next) => {
+  try {
+    const notifications =
+      await notificationService.findNotificationsByReceiverId(
+        req.params.receiverId
+      );
+    res.status(HTTP_STATUS.OK).json(notifications);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const notification = await notificationService.updateNotification(
+      req.params.id,
+      req.body
+    );
+    res.status(HTTP_STATUS.OK).json(notification);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const notification = await notificationService.deleteNotification(
+      req.params.id
+    );
+    res.status(HTTP_STATUS.OK).json(notification);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;

2dc137b8688154eabd6c113a822d2eb97b182ca9
OmarMuhammed03 <omarb2515@gmail.com>
2025-05-30 19:24:33 +0300

    feat(Auth): add Swagger documentation for authentication endpoints

---
+++ auth-service/docs/auth.swagger.js
@@ -0,0 +1,125 @@
+/**
+ * @swagger
+ * tags:
+ *   name: Auth
+ *   description: Authentication and authorization
+ */
+
+/**
+ * @swagger
+ * /auth/register:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/AuthUser'
+ *     responses:
+ *       201:
+ *         description: User registered
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/AuthUser'
+ *
+ * /auth/login:
+ *   post:
+ *     summary: Login a user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: User logged in
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 accessToken:
+ *                   type: string
+ *                 refreshToken:
+ *                   type: string
+ *
+ * /auth/logout:
+ *   post:
+ *     summary: Logout a user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               refreshToken:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: User logged out
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *
+ * /auth/refresh-token:
+ *   post:
+ *     summary: Refresh access token
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               refreshToken:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: New access token
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 accessToken:
+ *                   type: string
+ *
+ * components:
+ *   schemas:
+ *     AuthUser:
+ *       type: object
+ *       properties:
+ *         _id:
+ *           type: string
+ *         name:
+ *           type: string
+ *         email:
+ *           type: string
+ *         password:
+ *           type: string
+ *         birthDate:
+ *           type: string
+ *           format: date
+ *         gender:
+ *           type: string
+ *         createdAt:
+ *           type: string
+ *           format: date-time
+ */
--- auth-service/index.js
+++ auth-service/index.js
@@ -4,6 +4,8 @@ const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 dotenv.config();
 const colors = require("colors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
 
 const app = express();
 const authRouter = require("./controllers/authController");
@@ -14,10 +16,27 @@ connectDB(process.env.MONGO_URI).then(() =>
   console.log("Connected to MongoDB 200 OK".bgGreen.bold)
 );
 
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Auth Service API",
+      version: "1.0.0",
+      description: "API documentation for the Auth Service",
+    },
+    servers: [
+      { url: `http://localhost:${process.env.AUTH_SERVICE_PORT || 3001}` },
+    ],
+  },
+  apis: ["./docs/auth.swagger.js"],
+};
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.use("/auth", authRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use((err, req, res, next) => {
   const status = err.status || HTTP_STATUS.SERVER_ERROR;
